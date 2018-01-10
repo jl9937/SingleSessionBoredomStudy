4a08a1fea890b885027980ff5b0103a37e842a67
jl9937 <jim.lumsden@gmail.com>
2018-01-10 18:34:54 +0000

    New staircasing algorithm
    Far simpler

--- js/helpers/Staircase.js
+++ js/helpers/Staircase.js
@@ -2,7 +2,6 @@
 {
     this.startingSSD = _startingSSD;
     this.SSD = this.startingSSD;
-    this.previousTrialCorrect = undefined;
     this.step = 50;
     this.reversals = 0;
     this.direction = 0;
@@ -12,56 +11,32 @@ Staircase.prototype.getSSD = function ()
 {
     return this.SSD;
 }
-
-
-Staircase.prototype.adjust = function(currentTrialCorrect)
-{
-    var stepUp = undefined;
-    var newDirection = undefined;
-
-    //Determine staircase type and whether it should move up or down   
-    if (currentTrialCorrect)
-        stepUp = true;
-    else
-        stepUp = false;
-    
-      
+          
+Staircase.prototype.adjust = function (stepUp)
+{ 
     //Determine if there's been a change in direction
-    if (stepUp)
-    {
-        this.SSD = this.SSD + this.step;
-        newDirection = 1;
-    }
-    else
-    {
-        this.SSD = this.SSD - this.step;
-        newDirection = -1;
-    }
-    //If there has, record it and maybe act on it 
+    var newDirection = (stepUp===true) ? 1 : -1;
+
     if (newDirection !== this.direction)
     {
-        //debug("Staircase reversal");
-        this.reversals = this.reversals + 1;
-        this.direction = newDirection;
-        if (this.reversals > 4)
-        {
-           debug("Step size changed to 12");
+        this.reversals += 1;
+        
+        if (this.reversals > 5)
             this.step = 12;
-        }
-        else if (this.reversals > 2) {
-            debug("Step size changed to 25");
+        else if (this.reversals > 3)
             this.step = 25;
-        }
+
+        this.direction = newDirection;
     }
 
+    if (stepUp)
+        this.SSD += this.step;   
+    else 
+        this.SSD -= this.step;         
+   
     //Set the staircase to within bounds
-    if (this.SSD <= 0)
-    {
-        this.reversals = this.reversals + 1;
-        this.SSD = 25;
-    }
+    if (this.SSD <= 25)
+        this.SSD = 25;   
     if (this.SSD > 750)
-        this.SSD = 750;
-
-    this.previousTrialCorrect = currentTrialCorrect;
+        this.SSD = 750;        
 }
